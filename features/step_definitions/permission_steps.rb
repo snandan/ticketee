642b49312963221419dc37d40b1b84cbf92b52dd
Suman Nandan <suman.nandan@gmail.com>
2014-01-10 20:50:36 -0800

    Restricting ticket update to only who have permission.

--- features/step_definitions/permission_steps.rb
+++ features/step_definitions/permission_steps.rb
@@ -14,6 +14,17 @@ Given(/^"(.*?)" can create tickets in the "(.*?)" project$/) do |user, project|
                      :action => "create tickets")
 end
 
+Given(/^"(.*?)" can edit tickets in the "(.*?)" project$/) do |user, project|
+  Permission.create!(:user   => User.find_by_email!(user),
+                     :thing  => Project.find_by_name!(project),
+                     :action => "edit tickets")
+
+  Permission.create!(:user   => User.find_by_email!(user),
+                     :thing  => Project.find_by_name!(project),
+                     :action => "update tickets")
+  
+end
+
 
 # Given permission_step do |user, permission, on, project|
 #   create_permission(user, find_project(project), permission)
